e863c313da83803d5ae2cdeec9907b34e3bf7fc2
GitHub Action <action@github.com>
2023-03-13 02:42:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/cspanda8989/chatgptcodereviewplugin/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/cspanda8989/chatgptcodereviewplugin/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.cspanda8989.chatgptcodereviewplugin.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
